9e8364d874e58c2390dc8bec64bc6f9a1555ef06
Kevin Hall <kevinmhall8@gmail.com>
2020-05-28 18:16:41 -0700

    fixed errors and quiz fully works

---
+++ README.md
@@ -0,0 +1,13 @@
+# Code Quiz
+
+----
+## Instructions
+Follow the link to play a quiz game with 10 questions. Every question correct will add a point to your score. Every incorrect question will remove seconds from the timer. 
+
+Once you are done, add your name or initials to the high score page and compare your score with others. The High Score list sorts in order from highest to lowest score. 
+
+Sound Effects help recognize when a choice is correct or incorrect. 
+
+## Deployed Version
+
+https://kevinmhall8.github.io/Coding_Quiz/
--- script.js
+++ script.js
@@ -1,82 +1,211 @@
+
 var questions = [
-    {
-        title: "Inside which HTML element can Javascript be input?",
-        choices: ["<js>", "<scripting>", "<script>", "<javascript>"],
-        answer: 2
-      },
-      {
-        title: "What JavaScript syntax changes the content of an HTML element?",
-        choices: ["getElementByName", "getElementById", "getElementByStyle", "getElementByElement"],
-        answer: 1
-      },
-      {
-        title: "Where can a link to a javascript page be applied in HTML?",
-        choices: ["Head", "Head and Body", "Body", "Legs"],
-        answer: 1
-      },
-      {
-        title: "What is the correct syntax for referring to an external script called xxx.js?",
-        choices: ["<script src=\"xxx.js\">", "<script name=\"xxx.js\">", "<script href=\"xxx.js\">", "<script=\"xxx.js\">"],
-        answer: 0
-      },
-      {
-        title: "How do you write \"Hello World\" in an alert box?",
-        choices: ["msg(\"Hello World\"", "alertBox(\"Hello World\"", "alert(\"Hello World\"", "msgBox(\"Hello World\""],
-        answer: 1
-      },
-      {
-        title: "How do you create a function in JavaScript?",
-        choices: ["function myFunction()", "oh function, myFunction", "function = myFunction()", "function:myFunction"],
-        answer: 2
-      },
-      {
-        title: "How do you call a function named \"myFunction\"?",
-        choices: ["function.activate()", "call myFunction()", "call function myFunction()", "myFunction()"],
-        answer: 3
-      },
-      {
-        title: "How can you add a comment in JavaScript?",
-        choices: ["// ", "xx", "Comment: ", "ignoreThis"],
-        answer: 0
-      },
-      {
-        title: "Is JavaScript the same as Java?",
-        choices: ["Yes", "No", "Sometimes", "Only on Tuesdays"],
-        answer: 1
-      },  
-      {
-        title: "What event occurs when the user clicks on an HTML element?",
-        choices: ["onmouseover", "onmouseclick", "onclick", "onchange"],
-        answer: 2
-      },
-    ];
-    
+  {
+    title: "Inside which HTML element can Javascript be input?",
+    choiceA: "<js>",
+    choiceB: "<scripting>",
+    choiceC: "<script>",
+    choiceD: "<javascript>",
+    answer: "C"
+  },
+  {
+    title: "What JavaScript syntax changes the content of an HTML element?",
+    choiceA: "getElementByName",
+    choiceB: "getElementById",
+    choiceC: "getElementByStyle",
+    choiceD: "getElementByElement",
+    answer: "B"
+  },
+  {
+    title: "Where can a link to a javascript page be applied in HTML?",
+    choiceA: "Head",
+    choiceB: "Head and Body",
+    choiceC: "Body",
+    choiceD: "Legs",
+    answer: "B",
+  },
+  {
+    title: "What is the correct syntax for referring to an external script called xxx.js?",
+    choiceA: "<script src=\"xxx.js\">",
+    choiceB: "<script name=\"xxx.js\">",
+    choiceC: "<script href=\"xxx.js\">",
+    choiceD: "<script=\"xxx.js\">",
+    answer: "A"
+  },
+  {
+    title: "How do you write \"Hello World\" in an alert box?",
+    choiceA: "msg(\"Hello World\")",
+    choiceB: "alertBox(\"Hello World\")",
+    choiceC: "alert(\"Hello World\")",
+    choiceD: "msgBox(\"Hello World\")",
+    answer: "B"
+  },
+  {
+    title: "How do you create a function in JavaScript?",
+    choiceA: "function myFunction()",
+    choiceB: "oh function, myFunction",
+    choiceC: "function = myFunction()",
+    choiceD: "function:myFunction",
+    answer: "A"
+  },
+  {
+    title: "How do you call a function named \"myFunction\"?",
+    choiceA: "function.activate()",
+    choiceB: "call myFunction()",
+    choiceC: "call function myFunction()",
+    choiceD: "myFunction()",
+    answer: "D"
+  },
+  {
+    title: "How can you add a comment in JavaScript?",
+    choiceA: "// ",
+    choiceB: "xx",
+    choiceC: "Comment: ",
+    choiceD: "ignoreThisItIsNotImportantPleaseIgnore",
+    answer: "A"
+  },
+  {
+    title: "Is JavaScript the same as Java?",
+    choiceA: "Yes",
+    choiceB: "No",
+    choiceC: "Sometimes",
+    choiceD: "Asbestos",
+    answer: "B"
+  },
+  {
+    title: "What event occurs when the user clicks on an HTML element?",
+    choiceA: "onmouseover",
+    choiceB: "onmouseclick",
+    choiceC: "onclick",
+    choiceD: "onchange",
+    answer: "C"
+  },
+];
 
-let score = 0;
-let currentQuestion = 0;
+var score = 0;
+var currentQuestion = 0;
+var lastQuestion = questions.length - 1;
+var timeLeft = 30;
+var displayTimer = document.getElementById("timeGauge");
+var displayQuestion = document.getElementById("question-text");
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var choiceD = document.getElementById("D");
+var grade = document.getElementById("grade");
+var scoreList = document.getElementById("score-list");
+var orderedList = $("<ol>")
 
-$(document).ready(function() {
+var scores = [];
+var right = $("#right")
+var wrong = $("#wrong")
 
-$("#start-button").on("click", function(){
+var name = document.getElementById("name");
+var maxHighScores = 5;
+
+
+
+$(document).ready(function () {
+
+  $("#start-button").on("click", function () {
     $("#start-screen").hide();
     $("#quiz").show();
     showQuestion();
-});
+    countdown();
+    startTimer();
+  });
 
+})
+var timerId
+
+function startTimer() {
+  timerId = setInterval(countdown, 1000);
 }
-)
+
+function showScore() {
+  clearTimeout(timerId)
+  $("#quiz").hide();
+  $("#results-screen").show();
+  finalScore()
+}
 
 
-function showQuestion(){
-  let q = questions[currentQuestion];
-  questions.innerHTML = "<h2>" + q.title + "</h2> <br>";
-
-  $("#question-text").append(questions.innerHTML);
-  for (var a = 0; a < q.choices.length; a++) {
-    $("#choices-buttons").append("<button>" + q.choices[a] + "</button> <br>");
-
-    if (q.choices.string === answer) {
-      score += 1
-    } else {}
+function countdown() {
+  if (timeLeft <= 0) {
+    clearTimeout(timerId);
+    timesUp();
+  } else {
+    displayTimer.innerHTML = "<br /> <h5>Time Remaining: " + timeLeft + "</h5>";
+    timeLeft--;
   }
+}
+
+
+function finalScore() {
+  grade.innerHTML = "<h2>" + score + " out of 10 </h2><br /><h5>"
+}
+
+
+function showQuestion() {
+  let q = questions[currentQuestion];
+  displayQuestion.innerHTML = "<h3>" + q.title + "</h3> <br>";
+  choiceA.innerText = q.choiceA;
+  choiceB.innerText = q.choiceB;
+  choiceC.innerText = q.choiceC;
+  choiceD.innerText = q.choiceD;
+}
+
+
+$("#save").on("click", function () {
+  $("#results-screen").hide();
+  scores = JSON.parse(localStorage.getItem("high-scores")) || []
+  var initials = $(this).siblings("input").val();
+  // var newScore = score + ":" + initials;
+  var newScore = {initials:initials, score:score}
+  
+
+  //adding score to local storage and save
+  scores.push(newScore);
+  localStorage.setItem("high-scores", JSON.stringify(scores));
+  
+  scores.sort((a, b) => b.score - a.score);
+  scores.splice(5)
+  for (var i= 0; i < scores.length; i++) {
+  var litag = document.createElement("li");
+
+  litag.textContent = scores[i].initials+ ": " + scores[i].score;
+  scoreList.appendChild(litag);
+  }
+
+  console.log(scoreList)
+  $("#high-scores").show();
+})
+
+function timesUp() {
+  $("#quiz").hide();
+  $("#results-screen").show();
+  $("#out-of-time").show();
+  finalScore();
+}
+
+
+function checkAnswer(answer) {
+  if (answer == questions[currentQuestion].answer) {
+    score++;
+    right[0].play()
+  } else {
+    timeLeft = timeLeft - 3;
+    wrong[0].play()
+  }
+  if (currentQuestion < lastQuestion) {
+    currentQuestion++;
+    showQuestion();
+  } else {
+    showScore();
+  }
+}
+
+//new 5/8
+function compare(a, b) {
+  return (b.score - a.score);
 }
